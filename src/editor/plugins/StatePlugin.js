e9ee4f2671d279ed206e1f098cd5d48cfdafe4bb
Arun Praba K <arunprabak@Aruns-MacBook-Pro.local>
2022-06-05 07:53:20 +0530

    Editor on init function added

--- src/editor/plugins/StatePlugin.js
+++ src/editor/plugins/StatePlugin.js
@@ -2,13 +2,29 @@ import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { useEffect } from "react";
 
-export default function StatePlugin({ value = "", onChange = () => {} }) {
+const noop = () => {};
+
+const defaultState = { value: "", onChange: noop, onInit: noop };
+
+export default function StatePlugin({
+  value,
+  onChange,
+  onInit,
+} = defaultState) {
   const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    if (typeof onInit === "function") {
+      onInit(editor);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     if (value) {
       editor.setEditorState(editor.parseEditorState(value));
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [editor]);
 
   return <OnChangePlugin onChange={onChange} />;
